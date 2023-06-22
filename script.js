8c7071900afcafd86101d80258158254a3ea096a
Nima Abed <abodnima84@gmail.com>
2023-06-22 17:57:18 +0330

    Create Some Code

---
+++ script.js
@@ -0,0 +1,3 @@
+const apiKey ="edc228562ac0a8aa3116d41c0687cf56"
+const input = document.querySelector("top-banner form input")
+const url=`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`
