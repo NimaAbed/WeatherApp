b12bc9e5549c7a665578c4f95f112d93fd65cc22
Nima Abed <abodnima84@gmail.com>
2023-06-22 17:58:26 +0330

    Get Ul

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const apiKey ="edc228562ac0a8aa3116d41c0687cf56"
 const input = document.querySelector("top-banner form input")
 const form=document.querySelector(".top-banner form")
+const getUl = document.querySelector(".cities")
 const url=`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`
