77fb66d66ac6b05d123f09a0dc7395edae78f4a4
Nima Abed <abodnima84@gmail.com>
2023-06-22 18:05:15 +0330

    Create Func Submit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const apiKey ="edc228562ac0a8aa3116d41c0687cf56"
-const input = document.querySelector("top-banner form input")
+const input = document.querySelector(".top-banner form input")
 const form=document.querySelector(".top-banner form")
 const getUl = document.querySelector(".cities")
-const url=`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`
+
+form.addEventListener("submit",submitHandler)
+function submitHandler(e){
+    e.preventDefault()
+    console.log(input.value)
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(data=>{
+        console.log(data)
+    })
+}
